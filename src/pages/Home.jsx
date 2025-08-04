9ef6c731da1ef77ba7088f27ba4ef6c7ad476380
DEVID19 <devidbisen84@gmail.com>
2025-08-04 11:33:50 +0530

    fix: handle case-sensitive file import errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Carousel from "../components/Carousel";
 import MidBanner from "../components/MidBanner";
-import Features from "../components/features";
+import Features from "../components/Features";
 
 const Home = () => {
   return (
